9cfeb8464f43c72130de6de37f4481ce72be0bad
nbnlong2712 <nhatlonghn1203@gmail.com>
2022-03-05 00:31:14 +0700

    Complete UI for UpcomingScreen

---
+++ lib/screens/upcoming/upcoming_card.dart
@@ -0,0 +1,132 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_neumorphic/flutter_neumorphic.dart';
+
+class UpcomingCard extends StatelessWidget {
+  UpcomingCard({
+    Key? key,
+    required this.avatar,
+    required this.name,
+    required this.bookingDate,
+    required this.endTime,
+    required this.startTime,
+  }) : super(key: key);
+
+  String avatar;
+  String name;
+  String bookingDate;
+  String startTime;
+  String endTime;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(12.0),
+      child: Neumorphic(
+        style: NeumorphicStyle(
+          depth: 4,
+          intensity: 0.8,
+          color: const Color.fromRGBO(220, 228, 228, 130),
+          shadowDarkColor: Colors.black87,
+          lightSource: LightSource.topLeft,
+          shadowLightColorEmboss: Colors.white
+        ),
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            children: <Widget>[
+              Row(
+                children: <Widget>[
+                  Padding(
+                    padding: const EdgeInsets.all(5.0),
+                    child: CircleAvatar(
+                      backgroundImage: AssetImage(avatar),
+                      radius: 35,
+                    ),
+                  ),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Text(
+                        name,
+                        style: TextStyle(fontSize: 17, fontWeight: FontWeight.w700),
+                      ),
+                      Row(
+                        children: <Widget>[
+                          Text(bookingDate),
+                          Padding(
+                            padding: const EdgeInsets.symmetric(horizontal: 4.0),
+                            child: Chip(
+                              backgroundColor: const Color.fromRGBO(0, 187, 0, 220),
+                              label:
+                                  Text(startTime, style: TextStyle(color: Colors.green, fontWeight: FontWeight.w500)),
+                              shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(5),
+                                  side: BorderSide(width: 1, color: Colors.green)),
+                            ),
+                          ),
+                          const Text(" - "),
+                          Padding(
+                            padding: const EdgeInsets.symmetric(horizontal: 4.0),
+                            child: Chip(
+                              backgroundColor: Colors.orange.shade100,
+                              label: Text(endTime,
+                                  style: TextStyle(color: Colors.deepOrangeAccent, fontWeight: FontWeight.w500)),
+                              shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(5),
+                                  side: BorderSide(width: 1, color: Colors.deepOrangeAccent)),
+                            ),
+                          ),
+                        ],
+                      )
+                    ],
+                  )
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+                  NeumorphicButton(
+                    style: NeumorphicStyle(
+                      depth: 3,
+                      shape: NeumorphicShape.flat,
+                      intensity: 0.9,
+                      color: Colors.red.shade100,
+                    ),
+                    onPressed: () {},
+                    child: SizedBox(
+                      width: MediaQuery.of(context).size.width * 0.32,
+                      child: Center(
+                        child: Text(
+                          "Cancel",
+                          style: TextStyle(color: Colors.red),
+                        ),
+                      ),
+                    ),
+                  ),
+                  NeumorphicButton(
+                    style: NeumorphicStyle(
+                      depth: 3,
+                      shape: NeumorphicShape.flat,
+                      intensity: 0.9,
+                      color: Colors.green.shade100,
+                    ),
+                    onPressed: () {},
+                    child: SizedBox(
+                      width: MediaQuery.of(context).size.width * 0.32,
+                      child: Center(
+                        child: Text(
+                          "Go to meeting",
+                          style: TextStyle(color: Colors.green),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/screens/upcoming/upcoming_screen.dart
+++ lib/screens/upcoming/upcoming_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_lettutor/screens/upcoming/upcoming_card.dart';
 
 class UpcomingScreen extends StatelessWidget {
   static const router = "/upcoming-screen";
@@ -7,10 +8,44 @@ class UpcomingScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final _upcomingList = List.generate(
+        10,
+            (index) => UpcomingCard(
+            avatar: "assets/images/avatar_long.png",
+            name: "Nhat Long",
+            bookingDate: "Tue, 3/8/2022",
+            endTime: "23:00",
+            startTime: "23:30"));
+
     return Scaffold(
-      body: SafeArea(
+      backgroundColor: const Color.fromRGBO(220, 228, 228, 130),
+      appBar: AppBar(
+        title: const Text(
+          "Tutors",
+          style: TextStyle(color: Colors.black),
+        ),
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+      ),
+      body: SingleChildScrollView(
         child: Center(
-          child: Text("Upcoming Screen"),
+          child: SafeArea(
+            child: Flex(
+              direction: Axis.vertical,
+              children: <Widget>[
+                SizedBox(
+                  height: MediaQuery.of(context).size.height,
+                  width: MediaQuery.of(context).size.width,
+                  child: ListView.builder(
+                    itemBuilder: (BuildContext context, int index) {
+                      return _upcomingList[index];
+                    },
+                    itemCount: _upcomingList.length,
+                  ),
+                ),
+              ],
+            ),
+          ),
         ),
       ),
     );
